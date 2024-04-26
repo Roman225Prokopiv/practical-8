265d4262497a502f96042166ce32cfa71ee607e1
Roman225Prokopiv <167847572+Roman225Prokopiv@users.noreply.github.com>
2024-04-26 21:47:58 +0300

    commit cals(two)

--- calc.py
+++ calc.py
@@ -1,9 +1,12 @@
-first = 25
-second = 5
+first = 0
+second = 1
 
 def calc(a,b):
     res = a/b
-    print(res)
+    if res==0:
+        return 'error'
+    else:
+        print(res)
 
 
 calc(first, second)
